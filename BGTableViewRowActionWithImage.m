ec8e14a4b147931e4779d66dc00fe418ba541760
klauslanza <klauslanza@gmail.com>
2016-05-04 11:22:00 +0200

    Refactored new methods as multiple constructors.

--- BGTableViewRowActionWithImage.m
+++ BGTableViewRowActionWithImage.m
@@ -19,11 +19,41 @@
 
 @implementation BGTableViewRowActionWithImage
 
-+ (instancetype)rowActionWithStyle:(UITableViewRowActionStyle)style title:(NSString *)title backgroundColor:(UIColor *)backgroundColor image:(UIImage *)image forCellHeight:(NSUInteger)cellHeight andFittedWidth:(BOOL)isWidthFitted handler:(void (^)(UITableViewRowAction *, NSIndexPath *))handler
+#pragma mark - Derived constructors
+
++ (instancetype)rowActionWithStyle:(UITableViewRowActionStyle)style
+                             title:(NSString *)title
+                   backgroundColor:(UIColor *)backgroundColor
+                             image:(UIImage *)image
+                     forCellHeight:(NSUInteger)cellHeight
+                           handler:(void (^)(UITableViewRowAction *, NSIndexPath *))handler;
+{
+    return [self rowActionWithStyle:style title:title titleColor:[UIColor whiteColor] backgroundColor:backgroundColor image:image forCellHeight:cellHeight andFittedWidth:NO handler:handler];
+}
+
++ (instancetype)rowActionWithStyle:(UITableViewRowActionStyle)style
+                             title:(NSString *)title
+                        titleColor:(UIColor *)titleColor
+                   backgroundColor:(UIColor *)backgroundColor
+                             image:(UIImage *)image
+                     forCellHeight:(NSUInteger)cellHeight
+                           handler:(void (^)(UITableViewRowAction *, NSIndexPath *))handler
+{
+    return [self rowActionWithStyle:style title:title titleColor:titleColor backgroundColor:backgroundColor image:image forCellHeight:cellHeight andFittedWidth:NO handler:handler];
+}
+
++ (instancetype)rowActionWithStyle:(UITableViewRowActionStyle)style
+                             title:(NSString *)title
+                   backgroundColor:(UIColor *)backgroundColor
+                             image:(UIImage *)image
+                     forCellHeight:(NSUInteger)cellHeight
+                    andFittedWidth:(BOOL)isWidthFitted
+                           handler:(void (^)(UITableViewRowAction *, NSIndexPath *))handler
 {
     return [self rowActionWithStyle:style title:title titleColor:[UIColor whiteColor] backgroundColor:backgroundColor image:image forCellHeight:cellHeight andFittedWidth:isWidthFitted handler:handler];
-    
 }
+
+#pragma mark - Main constructor
 
 + (instancetype)rowActionWithStyle:(UITableViewRowActionStyle)style title:(NSString *)title titleColor:(UIColor *)titleColor backgroundColor:(UIColor *)backgroundColor image:(UIImage *)image forCellHeight:(NSUInteger)cellHeight andFittedWidth:(BOOL)isWidthFitted handler:(void (^)(UITableViewRowAction *, NSIndexPath *))handler
 {
