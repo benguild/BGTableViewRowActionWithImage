3dc1dfcf82e6da79b147408d61ee4642f51b8e21
Tom Kraina <tomas.kraina@gmail.com>
2016-09-05 22:59:39 +0200

    Add example project

---
+++ Example/Pods/Target Support Files/BGTableViewRowActionWithImage/BGTableViewRowActionWithImage-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "BGTableViewRowActionWithImage.h"
+
+FOUNDATION_EXPORT double BGTableViewRowActionWithImageVersionNumber;
+FOUNDATION_EXPORT const unsigned char BGTableViewRowActionWithImageVersionString[];
+
